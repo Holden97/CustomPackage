8e60e7fabf92a75201dac335b12e6e5e912e5524
Gyb <479276132@qq.com>
2022-06-10 13:20:25 +0800

    测试包

---
+++ Editor/DebugStringEditor.cs
@@ -0,0 +1,18 @@
+/// Author：GuoYiBo
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DebugStringEditor : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("!!!!!!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Runtime/DebugString.cs
@@ -0,0 +1,18 @@
+/// Author：GuoYiBo
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DebugString : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("!!!!!!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
